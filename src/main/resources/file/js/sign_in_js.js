5c3992d4928d6644e861080c978021db29dc6482
Mikka <mapyajapan@yahoo.co.jp>
2017-12-04 18:39:15 +0800

    更新

--- src/main/resources/file/js/sign_in_js.js
+++ src/main/resources/file/js/sign_in_js.js
@@ -1,30 +1,61 @@
 /**
- * 
+ * 进行后台验证
+ * 如果错误就返回错误信息
+ * 正确就跳转
  */
-$('#login').click(function(){
-	
-	var username = $('#username').val().trim()
-	var password = $('#password').val().trim()
-	if (username == ""||password == "") {
-		$('#showmessage').html("请完整填写用户名和密码")
-		return false;
-	}
-	$.ajax({
-		url:"/ajaxs/sign_in",
-		type:"POST",
-		data:{
-			username:username,
-			password:password
-		},
-		success:function(data){
-			if (data == true) {
-				$('#form_sign_in').submit()
-			}else{
-				$('#showmessage').html("用户或密码错误")
-				return false;
-			}
-		}
-	})
+$(document).ready(function() {
+    //开启弹出框
+    $('.modal').modal();
+    $('#textarea1').val('');
+    $('#textarea1').trigger('autoresize');
+});
+
+$("#sendinfo").click(function() {
+    var findusername = $("#findusername").val();
+    var findemail = $("#findemail").val();
+    var info = $("#textarea1").val();
+
+    $.ajax({
+        type: "POST",
+        url: "urlkjkjkj",
+        data: {
+            "findusername": findusername,
+            "findemail": findemail,
+            "info": info
+        },
+        dataType: "JSON",
+        success: function(response) {
+            Materialize.toast('发送成功！请等待一段时间，我收到后就会发送密码到您的邮箱。', 10000);
+        },
+        error: function() {
+            Materialize.toast('网络似乎有问题，发送失败！您可以稍等一会重新尝试发送。', 10000);
+        }
+    });
+
+});
+
+$('#login').click(function() {
+
+    var username = $('#username').val().trim()
+    var password = $('#password').val().trim()
+    if (username == "" || password == "") {
+        $('#showmessage').html("请完整填写用户名和密码")
+        return false;
+    }
+    $.ajax({
+        url: "/ajaxs/sign_in",
+        type: "POST",
+        data: {
+            username: username,
+            password: password
+        },
+        success: function(data) {
+            if (data == true) {
+                $('#form_sign_in').submit()
+            } else {
+                $('#showmessage').html("用户或密码错误")
+                return false;
+            }
+        }
+    })
 })
-
-
--- src/main/resources/file/js/sign_up.js
+++ src/main/resources/file/js/sign_up.js
@@ -1,3 +1,30 @@
 /**
- * 
+ * 进行后台验证
+ * 如果错误就返回错误信息
+ * 正确就跳转
  */
+var theusername = $("#username").val();
+var thepassword = $("#password").val();
+var theemail = $("#email").val();
+var thepageusername = $("#pageusername").val();
+var theurl = $("#url").val();
+
+$("#sign_upbut").click(function(e) {
+    $.ajax({
+        type: "POST",
+        url: "/sign_up",
+        data: {
+            "username": theusername,
+            "password": thepassword,
+            "email": theemail,
+            "pageusername": thepageusername,
+            "url": theurl
+        },
+        dataType: "JSON",
+        success: function(response) {
+            $.each(response, function(indexInArray, valueOfElement) {
+                valueOfElement.indexInArray
+            });
+        }
+    });
+});

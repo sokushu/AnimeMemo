c12c67a2c4d1cf7e02afa19b0582c4cd971f7fb8
Mirukk <mapyajapan@yahoo.co.jp>
2018-05-16 21:56:07 +0800

    更新

--- src/main/java/moe/neptunenoire/test/TheTest.java
+++ src/main/java/moe/neptunenoire/test/TheTest.java
@@ -1,30 +1,15 @@
 package moe.neptunenoire.test;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
-import javax.annotation.Resource;
-import javax.servlet.http.HttpSession;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import moe.neptunenoire.mysql.dao.MaiKissReo;
-import moe.neptunenoire.util.MD5Coding;
 
 @Controller
 @EnableAutoConfiguration
@@ -35,14 +20,6 @@ public class TheTest {
 
 	public String time = String.valueOf(Calendar.getInstance().getTimeInMillis());
 
-	public MaiKissReo session;
-
-	@Autowired
-	public TheTest(MaiKissReo session){
-		this.session = session;
-		session.Anime_FindAllAnime();
-	}
-
 	@RequestMapping(value = test, method = RequestMethod.GET)
 	@ResponseBody
 	public String adada(String id){
@@ -51,12 +28,5 @@ public class TheTest {
 		a.add("e");
 		return a.get(2);
 		
-	}
-	
-	@RequestMapping(value = test + "add", method = RequestMethod.GET)
-	@ResponseBody
-	public String name() {
-		session.setAttribute("OK", "let's go!!");
-		return "OK";
 	}
 }

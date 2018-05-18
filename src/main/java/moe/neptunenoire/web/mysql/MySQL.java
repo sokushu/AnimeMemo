fea3097cd81225110ea6440dd6f220b9e9b099cb
Mirukk <mapyajapan@yahoo.co.jp>
2018-05-19 01:11:24 +0800

    更新

--- src/main/java/moe/neptunenoire/web/mysql/MySQL.java
+++ src/main/java/moe/neptunenoire/web/mysql/MySQL.java
@@ -4,14 +4,9 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.DeleteProvider;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.InsertProvider;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.SelectProvider;
-import org.apache.ibatis.annotations.UpdateProvider;
 
 import moe.neptunenoire.web.table.Anime;
 import moe.neptunenoire.web.table.Users;

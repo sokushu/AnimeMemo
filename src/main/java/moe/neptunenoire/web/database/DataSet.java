33343049383dee18c96d2e73730bc0a8bbe7dd59
Mirukk <mapyajapan@yahoo.co.jp>
2018-05-23 00:32:12 +0800

    更新

--- src/main/java/moe/neptunenoire/web/database/DataSet.java
+++ src/main/java/moe/neptunenoire/web/database/DataSet.java
@@ -16,27 +16,27 @@ import moe.neptunenoire.web.mysql.MaiKissReo;
 public class DataSet {
 
 	/**
-	 *
+	 * 用户的数据
 	 */
 	private static List<Map<String, Object>> usersData = new ArrayList<>();
 
 	/**
-	 *
+	 * 启动时从数据读取的动画数据
 	 */
 	private static Stream<Map<String, Object>> animeData = null;
 
 	/**
-	 *
+	 * 启动后新添加的动画数据
 	 */
 	private static List<Map<String, Object>> newAnimeData = new ArrayList<>();
 
 	/**
-	 *
+	 * 保存的数据数量
 	 */
 	private static int SaveDataNum = 1000;
 
 	/**
-	 *
+	 * 保存用户数据
 	 * @param data
 	 */
 	public static void saveUsersData(Map<String, Object> data) {
@@ -51,7 +51,7 @@ public class DataSet {
 	}
 
 	/**
-	 *
+	 * 得到用户数
 	 * @return
 	 */
 	public static int getUserNum() {
@@ -59,7 +59,7 @@ public class DataSet {
 	}
 
 	/**
-	 *
+	 * 得到数据的数量
 	 * @return
 	 */
 	public static int getAnimeNum() {
@@ -67,8 +67,8 @@ public class DataSet {
 	}
 
 	/**
-	 *
-	 * @param data
+	 * 保存一个动画数据
+	 * @param data 动画的数据
 	 */
 	public static void saveAnimeData(Map<String, Object> data) {
 		synchronized (animeData) {
@@ -77,8 +77,8 @@ public class DataSet {
 	}
 
 	/**
-	 *
-	 * @param filter
+	 * 得到动画集合
+	 * @param filter 过滤规则
 	 * @return
 	 */
 	public static List<Map<String, Object>> getAnimeData(Predicate<? super Map<String, Object>> filter){
@@ -86,8 +86,8 @@ public class DataSet {
 	}
 
 	/**
-	 *
-	 * @param filter
+	 * 得到一部动画
+	 * @param filter 过滤规则
 	 * @return
 	 */
 	public static Map<String, Object> getAnimeOne(Predicate<? super Map<String, Object>> filter){
@@ -95,7 +95,7 @@ public class DataSet {
 	}
 
 	/**
-	 *
+	 * 设置保存数量
 	 * @param num
 	 */
 	public static void setSaveDataNum(int num) {
@@ -103,7 +103,7 @@ public class DataSet {
 	}
 
 	/**
-	 *
+	 * 数据操作类初始化 
 	 * @param maiKissReo
 	 */
 	@Autowired
@@ -112,7 +112,7 @@ public class DataSet {
 	}
 
 	/**
-	 *
+	 * 数据初始化
 	 * @param maiKissReo
 	 */
 	private void initData(MaiKissReo maiKissReo) {

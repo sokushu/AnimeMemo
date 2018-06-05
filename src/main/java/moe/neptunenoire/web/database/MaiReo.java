c682ebfd29518bd118ef3c48f55932ccccb0f093
Mirukk <mapyajapan@yahoo.co.jp>
2018-06-05 18:08:39 +0800

    g

--- src/main/java/moe/neptunenoire/web/database/MaiReo.java
+++ src/main/java/moe/neptunenoire/web/database/MaiReo.java
@@ -4,5 +4,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MaiReo {
-
+	public MaiReo() {
+		System.out.println("Hello World");
+		System.err.println("Hello World");
+	}
 }

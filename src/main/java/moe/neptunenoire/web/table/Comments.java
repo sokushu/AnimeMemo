6918e4af71eb4a094f5ed5566b44e4202f8f93c9
Mirukk <mapyajapan@yahoo.co.jp>
2018-06-06 09:17:21 +0800

    g

--- src/main/java/moe/neptunenoire/web/table/Comments.java
+++ src/main/java/moe/neptunenoire/web/table/Comments.java
@@ -8,9 +8,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
 
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.validator.constraints.NotEmpty;
 
 /**
  * 留言

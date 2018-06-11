938fe627a5f17a8165d50dad4c36d73c09900805
Mirukk <mapyajapan@yahoo.co.jp>
2018-06-11 15:01:52 +0800

    g

---
+++ src/test/java/moe/neptunenoire/test/IndexTest.java
@@ -0,0 +1,26 @@
+package moe.neptunenoire.test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class IndexTest {
+
+	@Before
+	public void start() {
+
+	}
+
+	@After
+	public void end() {
+
+	}
+
+	@Test
+	public void go() {
+
+	}
+
+}

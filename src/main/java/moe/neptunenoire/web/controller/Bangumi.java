f71104146e7ff5522f1ea6e50ac8102af6d0228d
Mirukk <mapyajapan@yahoo.co.jp>
2018-06-26 18:19:51 +0800

    g

--- src/main/java/moe/neptunenoire/web/controller/Bangumi.java
+++ src/main/java/moe/neptunenoire/web/controller/Bangumi.java
@@ -5,22 +5,16 @@ import java.util.Map;
 import org.springframework.data.redis.core.RedisTemplate;
 
 import moe.neptunenoire.web.controller.error.BangumiNotFoundException;
-import moe.neptunenoire.web.database.DataSet;
+import moe.neptunenoire.web.database.ReoKissMai;
 import moe.neptunenoire.web.mysql.MaiKissReo;
-import moe.neptunenoire.web.util.StringUtil;
 
 public class Bangumi {
 
-	/** 数据库字段 */
-	private MaiKissReo maiKissReo;
-	/** 内存数据库操作 */
-	private DataSet dataSet;
-	/** 字符工具类 */
-	private StringUtil stringUtil = new StringUtil();
+	/** 数据库操作 */
+	private ReoKissMai reoKissMai;
 
 	public Bangumi(MaiKissReo maiKissReo, RedisTemplate<String, Map<String, Object>> redis) {
-		this.maiKissReo = maiKissReo;
-		this.dataSet = new DataSet(maiKissReo, redis);
+		reoKissMai = new ReoKissMai(maiKissReo, redis);
 	}
 
 	/**
@@ -31,26 +25,7 @@ public class Bangumi {
 	 */
 	public Map<String, Object> showBangumi(String BangumiID) throws BangumiNotFoundException {
 
-		int bangumiid;
-		try {
-			/* 转换成数字 */
-			bangumiid = Integer.parseInt(BangumiID);
-		} catch (Exception e) {
-			throw new BangumiNotFoundException();
-		}
-
-		/* 数据库查询动画资料 */
-		Map<String, Object> bangumiData = dataSet.getAnimeOne(bangumiid);
-
-		/* 验证是否为空 */
-		if (stringUtil.isNull(bangumiData)) {
-			bangumiData = maiKissReo.Anime_FindByAnimeID(BangumiID);
-			dataSet.saveAnimeData(bangumiData);
-		}
-		/* 验证是否为空 */
-		if (stringUtil.isNull(bangumiData)) {
-			throw new BangumiNotFoundException();
-		}
+		Map<String, Object> bangumiData = reoKissMai.Anime_FindByAnimeID(BangumiID);
 
 		return bangumiData;
 	}

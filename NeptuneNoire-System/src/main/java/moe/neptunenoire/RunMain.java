88b72167e472c590ed8bf849c5ac38b24e083550
Mirukk <mapyajapan@yahoo.co.jp>
2018-07-11 23:15:13 +0800

    g

---
+++ NeptuneNoire-System/src/main/java/moe/neptunenoire/RunMain.java
@@ -0,0 +1,18 @@
+package moe.neptunenoire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动
+ * @author jo
+ *
+ */
+@SpringBootApplication
+public class RunMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunMain.class, args);
+	}
+	
+}

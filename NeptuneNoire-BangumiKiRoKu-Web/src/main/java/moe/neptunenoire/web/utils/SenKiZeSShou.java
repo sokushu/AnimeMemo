0d9877460297f2f012fb7deef3fd46311c796bde
Mirukk <mapyajapan@yahoo.co.jp>
2018-07-19 11:42:18 +0800

    g

--- NeptuneNoire-BangumiKiRoKu-Web/src/main/java/moe/neptunenoire/web/utils/SenKiZeSShou.java
+++ NeptuneNoire-BangumiKiRoKu-Web/src/main/java/moe/neptunenoire/web/utils/SenKiZeSShou.java
@@ -1,247 +1,253 @@
 package moe.neptunenoire.web.utils;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+
+import moe.neptunenoire.web.utils.SenKiZeSShou.Type;
 
 /**
  *
  * @author M
  *
  */
-public class SenKiZeSShou {
+public class SenKiZeSShou implements Narcissu{
+
+	/**
+	 *
+	 * @author M
+	 *
+	 * @param <T>
+	 */
+	public static class Type<T>{
+
+		private T value;
+		/**
+		 *
+		 * @param t
+		 */
+		public Type(T t) {
+			value = t;
+		}
+
+		/**
+		 *
+		 */
+		public Type() {}
+
+		/**
+		 *
+		 * @return
+		 */
+		public T getValue() {
+			return value;
+		}
+
+	}
 
 	/**
 	 *
 	 */
-	private Class<? extends Object> CVlass;
+	private Class<?> ClassObj;
 
 	/**
 	 *
 	 */
 	private Object obj;
 
-	/**
-	 *
-	 * @param class1
-	 * @param obj2
-	 */
-	private SenKiZeSShou(Class<? extends Object> class1, Object obj2) {
-		this.CVlass = class1;
-		obj = obj2;
-	}
+	private Map<String, Field> field;
+
+	private Map<String, Method> method;
+
+	private Map<String, Object> fieldValue;
 
 	/**
 	 *
+	 * @param ClassObj
 	 * @param obj
-	 * @return
 	 */
-	public static SenKiZeSShou loadClass(Object obj) {
-		return new SenKiZeSShou(obj.getClass(), obj);
+	private SenKiZeSShou(Class<?> ClassObj, Object obj) {
+		this.ClassObj = ClassObj;
+		this.obj = obj;
 	}
 
 	/**
 	 *
-	 * @param fieldsName
+	 * @param classObj
 	 * @return
 	 */
-	public Map<String, Object> getFields(String...fieldsName) {
-		Map<String, Object> data = new HashMap<>();
-		List<String> fieldName = Arrays.asList(fieldsName);
-		for (Field field : CVlass.getDeclaredFields()) {
-			field.setAccessible(true);
-			String name = field.getName();
-			if (fieldName.contains(name)) {
-				try {
-					data.put(name, field.get(obj));
-				} catch (IllegalArgumentException | IllegalAccessException e) {
-					e.printStackTrace();
+	public static SenKiZeSShou LoadClass(Object classObj) {
+		return new SenKiZeSShou(classObj.getClass(), classObj);
+	}
+
+	/**
+	 *
+	 */
+	@Override
+	public <T> T getField(String fieldName, Type<T> type) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	@Override
+	public Map<String, Object> getFields() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	@Override
+	public Map<String, Object> getFields(String... fieldNames) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	@Override
+	public <T> void setField(String fieldName, Object value, Type<T> type) {
+		Field field = this.field.get(fieldName);
+
+	}
+
+	@Override
+	public <T> T RunMethod(String methodName, Type<T> type, Object... Parameters) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	/**
+	 *
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 */
+	private void LoadFields() throws IllegalArgumentException, IllegalAccessException {
+		Class<?> theclass = ClassObj;
+		Map<String, Field> field = new HashMap<String, Field>() {
+			@Override
+			public Field put(String key, Field value) {
+				if (!this.containsKey(key)) {
+					return super.put(key, value);
+				}else {
+					return null;
 				}
 			}
+		};
+		Map<String, Object> fieldValue = new HashMap<String, Object>(){
+			@Override
+			public Object put(String key, Object value) {
+				if (!this.containsKey(key)) {
+					return super.put(key, value);
+				}else {
+					return null;
+				}
+			}
+		};
+		while (theclass != null) {
+			for (Field field1 : theclass.getDeclaredFields()) {
+				field1.setAccessible(true);
+				String fieldName = field1.getName();
+				field.put(fieldName, field1);
+				fieldValue.put(fieldName, field1.get(obj));
+			}
+			theclass = theclass.getSuperclass();
 		}
-		return data;
+		this.field = new HashMap<>(field);
+		this.fieldValue = new HashMap<>(fieldValue);
 	}
+
+	/**
+	 *
+	 */
+	private void LoadMethods() {
+		Class<?> theclass = ClassObj;
+		Map<String, Method> methods = new HashMap<String, Method>() {
+			@Override
+			public Method put(String key, Method value) {
+				if (!this.containsKey(key)) {
+					return super.put(key, value);
+				}else {
+					return null;
+				}
+			}
+		};
+		while (theclass != null) {
+			for (Method method : theclass.getDeclaredMethods()) {
+				String methodName = method.getName();
+				this.method.put(methodName, method);
+			}
+			theclass = theclass.getSuperclass();
+		}
+		this.method = new HashMap<>(methods);
+	}
+
+	/**
+	 *
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 */
+	private void ReLoad() throws IllegalArgumentException, IllegalAccessException {
+		LoadMethods();
+		LoadFields();
+	}
+
+	/**
+	 *
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 */
+	private void ChackReLoad() throws IllegalArgumentException, IllegalAccessException {
+		if (field == null || fieldValue == null) {
+			LoadFields();
+		}
+		if (method == null) {
+			LoadMethods();
+		}
+	}
+
+
+}
+
+/**
+ *
+ * @author M
+ *
+ */
+interface Narcissu{
 
 	/**
 	 *
 	 * @param fieldName
+	 * @param type
 	 * @return
-	 * @throws Exception
 	 */
-	public Object getField(String fieldName) throws Exception {
-		Class<?> theclass = CVlass;
-		while (theclass != null) {
-			for (Field field : theclass.getDeclaredFields()) {
-				field.setAccessible(true);
-				String name = field.getName();
-				if (fieldName.equals(name)) {
-					try {
-						return field.get(obj);
-					} catch (IllegalArgumentException | IllegalAccessException e) {
-						throw e;
-					}
-				}
-			}
-			theclass = theclass.getSuperclass();
-		}
-		throw new Exception(fieldName+" NOT Found");
-	}
+	<T> T getField(String fieldName, Type<T> type);
 
 	/**
 	 *
-	 * @param fieldName
 	 * @return
-	 * @throws NoSuchFieldException
 	 */
-	private Field getFieldObject(String fieldName) throws NoSuchFieldException {
-		Class<?> theclass = CVlass;
-		while (theclass != null) {
-			for (Field field : theclass.getDeclaredFields()) {
-				field.setAccessible(true);
-				String name = field.getName();
-				if (fieldName.equals(name)) {
-					return field;
-				}
-			}
-			theclass = theclass.getSuperclass();
-		}
-		throw new NoSuchFieldException(fieldName+" NOT Found");
-	}
+	Map<String, Object> getFields();
+
+	/**
+	 *
+	 * @param fieldNames
+	 * @return
+	 */
+	Map<String, Object> getFields(String...fieldNames);
 
 	/**
 	 *
 	 * @param fieldName
 	 * @param value
+	 * @param type
 	 */
-	public void setField(String fieldName, Object value) {
-		try {
-			Field field = getFieldObject(fieldName);
-			field.setAccessible(true);
-			field.set(obj, value);
-		} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
-			e.printStackTrace();
-		}
-	}
-
-	/**
-	 *
-	 */
-	public void printAllFieldName() {
-		List<Field> fieldList = new ArrayList<>();
-		Class<?> theclass = CVlass;
-		while (theclass != null) {
-			fieldList.addAll(Arrays.asList(theclass.getDeclaredFields()));
-			theclass = theclass.getSuperclass();
-		}
-		for (Field field : fieldList) {
-			System.out.println(field.getName()+" : Type = "+field.getType().getSimpleName());
-		}
-	}
-
-	/**
-	 *
-	 */
-	public void printAllMethodName() {
-		List<Method> fieldList = new ArrayList<>();
-		Class<?> theclass = CVlass;
-		while (theclass != null) {
-			fieldList.addAll(Arrays.asList(theclass.getDeclaredMethods()));
-			theclass = theclass.getSuperclass();
-		}
-		for (Method field : fieldList) {
-			System.out.println(field.getName()+" : Type = "+field.getReturnType().getSimpleName());
-		}
-	}
-
-	/**
-	 *
-	 * @param methodNames
-	 * @return
-	 */
-	public Map<String, Method> getMethods(String...methodNames) {
-		List<String> list = Arrays.asList(methodNames);
-		Map<String, Method> data = new HashMap<>();
-		for (Method var : CVlass.getDeclaredMethods()) {
-			var.setAccessible(true);
-			if (methodNames.length > 0) {
-				if (list.contains(var.getName())) {
-					data.put(var.getName(), var);
-				}
-			}else {
-				data.put(var.getName(), var);
-			}
-		}
-		return data;
-	}
-
-	/**
-	 *
-	 * @param fieldName
-	 * @throws Exception
-	 */
-	public void clearFieldValue(String fieldName) throws Exception {
-		Field field = getFieldObject(fieldName);
-		String fieldClassName = field.getType().getName();
-		if (fieldClassName.equals(Map.class.getName())) {
-			field.set(obj, new HashMap<>());
-		}else if (fieldClassName.equals(List.class.getName())) {
-			field.set(obj, new ArrayList<>());
-		}else if (fieldClassName.equals(String.class.getName())) {
-			field.set(obj, "");
-		}
-	}
+	<T> void setField(String fieldName, Object value, Type<T> type);
 
 	/**
 	 *
 	 * @param methodName
+	 * @param type
+	 * @param Parameters
 	 * @return
-	 * @throws Exception
 	 */
-	public Method getMethod(String methodName) throws Exception {
-		for (Method var : CVlass.getDeclaredMethods()) {
-			var.setAccessible(true);
-			if (methodName.equals(var.getName())) {
-				return var;
-			}
-		}
-		throw new Exception(methodName + " Not Found");
-	}
-
-	/**
-	 *
-	 * @param method
-	 * @param objects
-	 * @return
-	 * @throws IllegalAccessException
-	 * @throws IllegalArgumentException
-	 * @throws InvocationTargetException
-	 */
-	@Deprecated
-	public Object RunMethod(Method method, Object...objects) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-		return method.invoke(obj, objects);
-	}
-
-	/**
-	 *
-	 * @param methodName
-	 * @param objects
-	 * @return
-	 * @throws Exception
-	 */
-	public Object RunMethod(String methodName, Object...objects) throws Exception{
-		try {
-			Method method = getMethod(methodName);
-			return method.invoke(obj, objects);
-		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-			throw e;
-		} catch (Exception e) {
-			throw e;
-		}
-	}
-
+	<T> T RunMethod(String methodName, Type<T> type, Object...Parameters);
 }
--- NeptuneNoire-System/src/main/java/moe/neptunenoire/Process/FileProcess.java
+++ NeptuneNoire-System/src/main/java/moe/neptunenoire/Process/FileProcess.java
@@ -1,24 +1,122 @@
 package moe.neptunenoire.Process;
 
-import java.awt.Image;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 
-import javax.imageio.ImageIO;
-
+/**
+ *
+ * @author M
+ *
+ */
 public class FileProcess {
 
-	private File file;
-	
+	/**
+	 *
+	 */
+	private LinkedList<String> filePaths = new LinkedList<>();
+
+	/**
+	 *
+	 */
+	private StringBuilder sb = new StringBuilder();
+
+	/**
+	 *
+	 * @param path
+	 */
 	public FileProcess(String path) {
-		file = new File(path);
+		filePaths.addLast(path);
 	}
-	
-	public void test() {
-		try {
-			
-		} catch (Exception e) {
-			// TODO: handle exception
+
+	/**
+	 *
+	 * @param path
+	 */
+	public void appPath(String path) {
+		filePaths.addLast(sb.append(privateGetFilePath()).append(path).toString());
+	}
+
+	/**
+	 *
+	 */
+	public void deletePath() {
+		filePaths.set(filePaths.size()-1, null);
+	}
+
+	/**
+	 *
+	 * @param Path
+	 */
+	public void setPath(String Path) {
+		filePaths.addLast(Path);
+	}
+
+	/**
+	 *
+	 * @param path
+	 */
+	public void setPath(String[] path) {
+		for (String string : path) {
+			filePaths.add(string);
 		}
 	}
-	
+
+	public void setPath(Collection<? extends String> c) {
+		filePaths.addAll(c);
+	}
+
+	/**
+	 *
+	 * @return
+	 */
+	public boolean mkdirs() {
+		List<Boolean> flag = new ArrayList<Boolean>();
+		filePaths.forEach(filePath->{
+			flag.add(new File(filePath).mkdirs());
+		});
+		return !flag.contains(false);
+	}
+
+	/**
+	 *
+	 * @return
+	 */
+	private String privateGetFilePath() {
+		return filePaths.getLast();
+	}
+
+	/**
+	 *
+	 * @param file
+	 * @param deleteMyself
+	 * @return
+	 */
+	private boolean delete(File file, boolean deleteMyself) {
+		List<Boolean> flag = new ArrayList<Boolean>();
+		if (!file.exists()) return true;
+		for (File var : file.listFiles()) {
+			if (var.isDirectory()) {
+				delete(var, deleteMyself);
+			}
+			flag.add(var.delete());
+		}
+		return !flag.contains(false);
+	}
+
+	/**
+	 *
+	 * @param deleteMyself
+	 * @return
+	 */
+	public boolean delete(boolean deleteMyself) {
+		List<Boolean> flag = new ArrayList<Boolean>();
+		filePaths.forEach(filePath->{
+			flag.add(delete(new File(filePath), deleteMyself));
+		});
+		return !flag.contains(false);
+	}
+
 }
